d155c819069f7d73fe6d00587a58898fbbc7b311
sutaC <catus.pl@gmail.com>
2024-07-10 16:49:14 +0200

    Author form component

---
+++ src/lib/components/AuthorForm.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import type { Author } from '$lib/types';
+
+	export let author: Author = {
+		id: 0,
+		name: ''
+	};
+
+	export let action: string = '';
+	export let method: string = '';
+</script>
+
+<form {action} {method}>
+	<div class="field">
+		<label for="name">Name</label>
+		<input type="text" name="name" id="name" bind:value={author.name} />
+	</div>
+	<button type="reset">Reset</button>
+	<button type="submit">Submit</button>
+</form>
+
+<style>
+	form {
+		padding: 0.5rem;
+		margin: 0.5rem;
+		border: 1px solid #000;
+	}
+
+	.field {
+		margin: 0.5rem 0;
+	}
+
+	button {
+		margin: 0.5rem 0;
+	}
+</style>
--- src/routes/author/[id]/edit/+page.svelte
+++ src/routes/author/[id]/edit/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import AuthorForm from '$lib/components/AuthorForm.svelte';
 	import type { PageData } from './$types';
 	export let data: PageData;
 </script>
@@ -8,27 +9,4 @@
 
 <h1>Edit author</h1>
 
-<form action={`/author/${data.id}/edit`} method="post">
-	<div class="field">
-		<label for="name">Name</label>
-		<input type="text" name="name" id="name" bind:value={data.name} />
-	</div>
-	<button type="reset">Reset</button>
-	<button type="submit">Submit</button>
-</form>
-
-<style>
-	form {
-		padding: 0.5rem;
-		margin: 0.5rem;
-		border: 1px solid #000;
-	}
-
-	.field {
-		margin: 0.5rem 0;
-	}
-
-	button {
-		margin: 0.5rem 0;
-	}
-</style>
+<AuthorForm action={`/author/${data.id}/edit`} method={'post'} author={data} />
