df05324bbf55d2e3fbe901efdbe9256d286fa492
sutaC <catus.pl@gmail.com>
2024-07-10 13:48:12 +0200

    Song page base styling

--- src/routes/song/[id]/+page.svelte
+++ src/routes/song/[id]/+page.svelte
@@ -7,14 +7,38 @@
 
 <h1>{data.title} - {data.author}</h1>
 
-<small>{data.id}</small>
+<main>
+	<div class="lyrics">
+		<p>Lyrisc</p>
+		<pre>{data.lyrics}</pre>
+	</div>
 
-<div>
-	<p>Lyrisc</p>
-	<pre>{data.lyrics}</pre>
-</div>
+	<div class="chords">
+		<p>Chords:</p>
+		<pre>{data.chords}</pre>
+	</div>
+</main>
 
-<div>
-	<p>Chords:</p>
-	<pre>{data.chords}</pre>
-</div>
+<style>
+	main {
+		display: flex;
+		padding: 1rem;
+		gap: 1rem;
+	}
+
+	main p {
+		font-weight: bold;
+	}
+
+	main pre {
+		text-wrap: wrap;
+	}
+
+	.lyrics {
+		flex: 4;
+	}
+
+	.chords {
+		flex: 1;
+	}
+</style>
